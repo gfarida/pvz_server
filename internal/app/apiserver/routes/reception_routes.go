fc34c48590917ae7706a7057cecef46a5e27d1cd
gfarida <faridafarida378@gmail.com>
2025-04-14 21:13:11 +0300

    Added close last reception route

--- internal/app/apiserver/routes/reception_routes.go
+++ internal/app/apiserver/routes/reception_routes.go
@@ -8,9 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func registerReceptionRoutes(r *gin.Engine, d *deps.Dependencies) {
+func registerReceptionRoutes(r *gin.Engine, deps *deps.Dependencies) {
 	protected := r.Group(("/"))
 	protected.Use(middleware.AuthMiddleware())
 
-	protected.POST("/receptions", handlers.CreateReception(d.Store))
+	protected.POST("/receptions", handlers.CreateReception(deps.Store))
+	protected.POST("/pvz/:pvzId/close_last_reception", handlers.CloseLastReception(deps.Store))
 }
